d5669539a3f1f8b20ee21cf75821007f7ccd11b6
Philip Roche <phil.roche@canonical.com>
2020-11-09 13:57:18 +0000

    Bump to 0.7.1 to publish changes in README

--- ubuntu_cloud_image_changelog/setup.py
+++ ubuntu_cloud_image_changelog/setup.py
@@ -43,6 +43,6 @@ setup(
         include=["ubuntu_cloud_image_changelog", "ubuntu_cloud_image_changelog.*"]
     ),
     url="https://github.com/CanonicalLtd/ubuntu_cloud_image_changelog",
-    version="0.7",
+    version="0.7.1",
     zip_safe=False,
 )
