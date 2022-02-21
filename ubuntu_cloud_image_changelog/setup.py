1d10bfa47e1a7b195d15a75a82e8ff6fab84e3f0
Philip Roche <phil.roche@canonical.com>
2022-02-21 23:40:07 +0000

    fix: fix bad bumpversion string replacements

--- ubuntu_cloud_image_changelog/setup.py
+++ ubuntu_cloud_image_changelog/setup.py
@@ -43,6 +43,6 @@ setup(
         include=["ubuntu_cloud_image_changelog", "ubuntu_cloud_image_changelog.*"]
     ),
     url="https://github.com/CanonicalLtd/ubuntu_cloud_image_changelog",
-    version="version='0.9.3'",
+    version="0.9.3",
     zip_safe=False,
 )
